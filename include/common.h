6d86cb71fbd01a03ad7323ba7fbadc603ed57117
Andrew Widdersheim <awiddersheim@hotmail.com>
2015-05-12 13:05:04 -0400

    Prep for 2.16 release

--- include/common.h
+++ include/common.h
@@ -2,7 +2,7 @@
  *
  * COMMON.H - NRPE Common Include File
  * Copyright (c) 1999-2007 Ethan Galstad (nagios@nagios.org)
- * Last Modified: 09-06-2013
+ * Last Modified: 05-12-2015
  *
  * License:
  *
@@ -23,8 +23,8 @@
 
 #include "config.h"
 
-#define PROGRAM_VERSION "2.15"
-#define MODIFICATION_DATE "09-06-2013"
+#define PROGRAM_VERSION "2.16"
+#define MODIFICATION_DATE "05-12-2015"
 
 #define OK		0
 #define ERROR		-1
